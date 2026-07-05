d456d15a2e13067a65f39758723964f11b565901
Shafa Islam <shafaislam050@gmail.com>
2026-07-05 12:08:23 +0600

    fix cors for production frontend

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+     origin: [
+      "http://localhost:5173",
+      "https://my-portfolio-mm0v.onrender.com/"
+    ],
     credentials: true,
   })
 );
